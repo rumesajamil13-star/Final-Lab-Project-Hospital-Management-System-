5298cf779cc5512544599ca57d197d0ad8a76fef
rumesajamil13-star <rumesajamil13@gmail.com>
2026-05-08 22:17:16 -0700

    .

--- file.cpp
+++ file.cpp
@@ -7,9 +7,7 @@
 #include <iomanip>
 #include <limits>
 #include <ctime>
-
 using namespace std;
-
 void clearScreen() 
 {
 #ifdef _WIN32
@@ -18,14 +16,12 @@ void clearScreen()
     system("clear");
 #endif
 }
-
 void pauseScreen()
 {
     cout << "\n  Press Enter to continue...";
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
     cin.get();
 }
-
 string getCurrentDate()
 {
     time_t now = time(0);
@@ -36,7 +32,74 @@ string getCurrentDate()
         << setw(2) << setfill('0') << l  tm->tm_mday;
     return oss.str();
 }
-
-void printLine(char c = '-', int width = 60) {
+void printLine(char c = '-', int width = 60)
+{
     cout << "  " << string(width, c) << "\n";
 }
+void printHeader(const string& title) {
+    clearScreen();
+    cout << "\n";
+    printLine('=');
+    cout << "  " << setw(60) << left << ("  HOSPITAL MANAGEMENT SYSTEM") << "\n";
+    printLine('=');
+    cout << "  >> " << title << "\n";
+    printLine();
+    cout << "\n";
+}
+class Person
+{
+protected:
+    string id;
+    string name;
+    int   age;
+    string gender;
+    string phone;
+public:
+    Person(const string& id, const string& name, int age,
+        const string& gender, const string& phone)
+        : id(id), name(name), age(age), gender(gender), phone(phone)
+    {
+    }
+    virtual ~Person() 
+    {
+    }
+    string getId() const
+    {
+        return id; 
+    }
+    string getName() const 
+    {
+        return name;
+    }
+    int    getAge() const 
+    {
+        return age; 
+    }
+    string getGender() const 
+    {
+        return gender;
+    }
+    string getPhone() const
+    {
+        return phone;
+    }
+    void setName(const string& n)
+    {
+        name = n; 
+    }
+    void setAge(int a) 
+    {
+        age = a;
+    }
+    void setGender(const string& g) 
+    {
+        gender = g;
+    }
+    void setPhone(const string& p)
+    {
+        phone = p; 
+    }
+    virtual void displayInfo()  const = 0;
+    virtual void saveToFile(ofstream& ofs) const = 0;
+    virtual string getType()    const = 0;
+};
